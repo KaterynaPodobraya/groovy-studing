0e64da727242eaabd7805b3892fd5d04b8049307
kateryna <kateryna.podobraya@gmail.com>
2020-04-15 09:32:45 +0300

    Groovy spread operator examples

---
+++ src/main/java/operators/SpreadOperator.groovy
@@ -0,0 +1,25 @@
+package operators
+
+class SpreadOperator{
+
+   static List spreadList(List list){
+       [1, 2, 3, 't', *list, "test"]
+
+   }
+   static spreadMap(Map map){
+       [1: "v1", *:map, 2: "v2"]
+   }
+
+    static spreadArguments(String s1, String s2, String s3){
+        println s1 + " " + s2 + " " +s3
+    }
+
+   public static void main(String[] args) {
+       def toSpread = [99, 100, 500]
+       println (spreadList(toSpread))
+       def mapToSpread = ["test1":"test val1"]
+       println (spreadMap(mapToSpread))
+       def arguments = ["Teddy", "loves", "honey"]
+       spreadArguments(*arguments)
+   }
+}
