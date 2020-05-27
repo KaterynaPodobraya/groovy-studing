c98d5fd5e25a5f1104f8aa939822290f835ad562
kateryna <kateryna.podobraya@gmail.com>
2020-05-27 08:42:07 +0300

    Adds examples for Range

--- src/main/java/operators/RangeOperator.groovy
+++ src/main/java/operators/RangeOperator.groovy
@@ -1,7 +1,7 @@
 package operators
 
 class RangeOperator {
-
+// http://docs.groovy-lang.org/latest/html/gapi/groovy/lang/Range.html
    static void main(String[] args) {
    // a groovy.lang.Range implements the List interface
        def range = 0..5
@@ -11,5 +11,18 @@ class RangeOperator {
        assert range instanceof List
        assert (0..5).size() == 6
        assert ('a'..'d').collect() == ['a','b','c','d']
+
+       assert range.containsWithinBounds(6) == false
+       assert range.containsWithinBounds(4)
+
+       assert range.getFrom() == 0
+       assert range.getTo() == 5
+
+       println(range.inspect())
+
+       def rangeReverse = 5..0
+       assert rangeReverse.isReverse()
+
+       println(rangeReverse.step(2))
    }
 }
