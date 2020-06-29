b3d89c74423f06ef23cd2d54f7ac98500927cf88
kateryna <kateryna.podobraya@gmail.com>
2020-06-29 12:52:58 +0300

    Modifies SwitchExample

--- src/main/groovy/operators/SwitchExample.groovy
+++ src/main/groovy/operators/SwitchExample.groovy
@@ -3,23 +3,23 @@ package operators
 class SwitchExample {
 
      static void main(String[] args) {
-        check(30)
-   //     println("test")
+        check(30.9999f)
+        check("Test")
     }
 
-    private static void check(int c){
+    private static void check(def c){
         switch (c) {
             case 10:
                 println (10 * 2)
                 break
-            case 25:
+            case "Test":
                 println(c)
                 break
             case 1..10:
                 println(1..10)
                 break
             case Float:
-                println("parameter is an Integer")
+                println("parameter is a Float")
                 break
             case [10, 20, 30]:
                 println(c)
