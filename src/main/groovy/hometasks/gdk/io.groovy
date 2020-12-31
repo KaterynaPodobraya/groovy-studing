640f734601dda42613fb42562134bd2522c98b06
kateryna <kateryna.podobraya@gmail.com>
2020-12-31 12:32:12 +0200

    System in

---
+++ src/main/groovy/hometasks/gdk/io.groovy
@@ -0,0 +1,11 @@
+package hometasks.gdk
+
+println "Please Enter Your Favorite Sports Team"
+
+String team
+System.in.withReader { reader ->
+    team = reader.readLine()
+}
+
+println "Your favorite team is: $team"
+
